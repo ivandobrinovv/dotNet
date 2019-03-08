7bcbfb88047e19b959f668cf952e3f08629c5106
Ivan Peev <idobrinovv@gmail.com>
2019-03-08 12:26:59 +0200

    Add MvcSample - simple mvc project without db connection

---
+++ MvcSample/MvcSample/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using MvcSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSample.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Index()
+        {
+            User[] users = new User[]
+            {
+                new User
+                {
+                    Name = "Petar",
+                    Surename = "Petrov"
+                },
+
+                new User
+                {
+                    Name = "Ivan",
+                    Surename = "Petrov"
+                },
+
+                new User
+                {
+                    Name = "Georgi",
+                    Surename = "Ivanov"
+                },
+            };
+
+            return View(users);
+        }
+
+        public ActionResult ShowUser()
+        {
+            User user = new User
+            {
+                Name = "Sergei",
+                Surename = "Simeonov"
+            };
+
+            return View(user);
+        }
+    }
+}
